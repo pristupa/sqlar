b1171bc6e1751ed971562b2b6cc2f03e8448f4ce
Pavel Pristupa <pristupa@gmail.com>
2019-10-22 23:23:22 +0700

    Make Repository abstract

--- src/repository.py
+++ src/repository.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from abc import abstractmethod
 from typing import Generic
 from typing import TypeVar
 
@@ -6,23 +6,40 @@ T = TypeVar('T')
 K = TypeVar('K')
 
 
-class Repository(Generic[T, K]):
-    def __init__(self):
-        self._identity_map: Dict[K, T] = {}
-
-    def get(self, id_: K) -> T:
-        return self._identity_map[id_]
-
-
 class MyEntity:
     def __init__(self, id_: int):
         self.id = id_
 
 
+def fetch_from_database(id_: int) -> MyEntity:
+    return MyEntity(id_)
+
+
+class Repository(Generic[T, K]):
+    @abstractmethod
+    def get(self, id_: K) -> T:
+        pass
+
+
+def sqla_repository(repository_class):
+    class RepositoryImpl:
+        def __init__(self):
+            self._identity_map = {}  # TODO: WeakRef?
+
+        def get(self, id_):
+            instance = self._identity_map.get(id_)
+            if instance is None:
+                self._identity_map[id_] = instance = fetch_from_database(id_)
+            return instance
+
+    # TODO: inspect repository_class interface to generate more methods
+
+    return RepositoryImpl
+
+
+@sqla_repository
 class MyRepository(Repository[MyEntity, int]):
-    def __init__(self):
-        super().__init__()
-        self._identity_map[42] = MyEntity(42)
+    pass
 
 
 my_repo = MyRepository()
