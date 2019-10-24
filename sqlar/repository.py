771eeaa2cbfad0807dcec5be6bd65dfac19009e7
Pavel Pristupa <pavel.pristupa@quantumsoft.ru>
2019-10-24 17:09:45 +0700

    Add tests for CRUDRepository

--- sqlar/repository.py
+++ sqlar/repository.py
@@ -1,17 +1,99 @@
-def sqla_crud(for_entity):
-    def wrapper(repository_cls):
-        class RepositoryImpl:
-            def __init__(self):
-                self._identity_map = {}  # TODO: WeakRef?
+from persipy import CRUDRepository
+from sqlalchemy import func
+from sqlalchemy import select
+from sqlalchemy.engine import Engine
+from sqlalchemy.orm import class_mapper
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import UnmappedClassError
+from typing import Iterable
+from typing import Optional
+from typing import TypeVar
 
-            def get(self, id_):
-                instance = self._identity_map.get(id_)
-                if instance is None:
-                    self._identity_map[id_] = instance = for_entity(id_)
-                return instance
+T = TypeVar('T')
+K = TypeVar('K')
 
-        # TODO: inspect repository_cls interface to generate more methods
 
-        return RepositoryImpl
+def sqla_crud(repository_cls):
+    if not issubclass(repository_cls, CRUDRepository):
+        raise TypeError('Repository must be inherited from CRUDRepository before annotating with sqla_crud')
 
-    return wrapper
+    entity_cls = repository_cls.__entity_cls__
+
+    try:
+        mapper = class_mapper(entity_cls)
+    except UnmappedClassError:
+        raise TypeError('Invalid SQLAlchemy entity class given')
+
+    if len(mapper.tables) > 1:
+        raise TypeError('sqla_crud does not support entities mapped to multiple tables')
+
+    entity_table = mapper.tables[0]
+
+    class RepositoryImpl(repository_cls):
+        """
+        SQLAlchemy implementation for CRUDRepository (persipy)
+        This repository implementation is not thread-safe.
+        """
+        class RepositoryException(Exception):
+            pass
+
+        def count(self) -> int:
+            return self._bind.execute(select([func.count()]).select_from(entity_table)).scalar()
+
+        def delete(self, entity: T):
+            pass
+            # self._session.delete(entity)
+            # self._session.flush()
+
+        def delete_many(self, entities: Iterable[T]):
+            pass
+            # self._session
+
+        def delete_all(self):
+            pass
+
+        def delete_by_id(self, id_: K):
+            pass
+
+        def exists_by_id(self, id_: K) -> bool:
+            pass
+
+        def find_all(self) -> Iterable[T]:
+            pass
+
+        def find_all_by_id(self, ids: Iterable[K]) -> Iterable[T]:
+            pass
+
+        def find_by_id(self, id_: K) -> Optional[T]:
+            pass
+            # if id_ in self._identity_map:
+            #     return self._identity_map[id_]
+            #
+            # session = self._session_factory()
+            # instance = session.query(entity_cls).get(id_)
+            # if instance is None:
+            #     return None
+            #
+            # self._identity_map[id_] = instance
+            # self._sessions[instance] = session
+            # return instance
+
+        def save(self, entity: T) -> T:
+            pass
+            # if entity not in self._sessions:
+            #     raise self.RepositoryException(f'Entity must be fetched with repository before saving: {entity}')
+            # self._sessions[entity].flush()
+            # return entity
+
+        def save_many(self, entities: Iterable[T]) -> Iterable[T]:
+            pass
+
+        def __init__(self, bind: Engine):
+            self._bind = bind
+            self._session_factory = sessionmaker(bind=self._bind)
+            self._identity_map = {}
+            self._sessions = {}
+
+    # TODO: inspect repository_cls interface to generate more methods
+
+    return RepositoryImpl
--- tests/test_sqla_crud.py
+++ tests/test_sqla_crud.py
@@ -1,4 +1,10 @@
-from persipy.repository import Repository
+from persipy import CRUDRepository
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import MetaData
+from sqlalchemy import Table
+from sqlalchemy import create_engine
+from sqlalchemy.orm import mapper
 
 from sqlar.repository import sqla_crud
 
@@ -8,12 +14,42 @@ class MyEntity:
         self.id = id_
 
 
-@sqla_crud(for_entity=MyEntity)
-class MyRepository(Repository[MyEntity, int]):
-    pass
+class Fixture:
+    def __init__(self):
+        self._engine = create_engine('sqlite://')
+        metadata = MetaData()
+        my_entity_table = Table('my_entities', metadata, Column('id', Integer, primary_key=True))
+        mapper(MyEntity, my_entity_table)
+
+        @sqla_crud
+        class MyRepository(CRUDRepository[MyEntity, int]):
+            pass
+
+        metadata.create_all(bind=self._engine)
+        self.repository = MyRepository(bind=self._engine)
+
+    def execute(self, sql):
+        return self._engine.execute(sql)
 
 
-def test_sqla_crud():
-    my_repo = MyRepository()
-    my_entity = my_repo.get(42)
-    assert my_entity.id == 42
+def test_count():
+    fixture = Fixture()
+    fixture.execute('INSERT INTO my_entities (id) VALUES (1), (2), (3);')
+
+    # Act
+    count = fixture.repository.count()
+
+    assert count == 3
+
+
+def test_delete():
+    fixture = Fixture()
+    fixture.execute('INSERT INTO my_entities (id) VALUES (1), (2);')
+    entity = fixture.repository.find_by_id(1)
+
+    # Act
+    fixture.repository.delete(entity)
+
+    result = fixture.execute('SELECT * FROM my_entities;')
+    assert result == [(2,)]
+    assert fixture.repository.find_by_id(1) is None
