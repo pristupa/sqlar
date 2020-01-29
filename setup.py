8eeb59f9b74f58d4368339aac28c84532b247543
Pavel Pristupa <pavel.pristupa@quantumsoft.ru>
2020-01-29 17:33:41 +0700

    Fix bug with saving new entities

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sqlar",
-    version="0.1.0",
+    version="0.2.0",
     author="Pavel V. Pristupa",
     author_email="pristupa@gmail.com",
     description="SQLAlchemy implementation for Python Persistance API (persipy)",
